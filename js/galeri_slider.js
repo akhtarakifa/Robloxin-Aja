9f8498e23e7ba252fd7015b6149de2733de4e767
uppermist <akhtarakifa57@gmail.com>
2026-02-20 10:39:47 +0700

    perbaikan_sedikit_pada_galeri_silder

--- js/galeri_slider.js
+++ js/galeri_slider.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const hasIncompleteCard = cardRefs.some(
-    (card) => !card.image || !card.title || !card.description,
+    (card) => !card.image || !card.title || !card.description
   );
 
   if (hasIncompleteCard) {
@@ -193,8 +193,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   prevBtn.addEventListener("click", () => {
-    currentSetIndex =
-      (currentSetIndex - 1 + gallerySets.length) % gallerySets.length;
+    currentSetIndex = (currentSetIndex - 1 + gallerySets.length) % gallerySets.length;
     renderSet(currentSetIndex);
   });
 
